6324eaba38b74c564898cea20199994e46edfbfe
LTH <johtta888@naver.com>
2022-05-02 18:23:39 +0900

    add startUp api

--- src/api/start-up/index.js
+++ src/api/start-up/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const checkLoggedIn = require("../../lib/checkLoggedIn");
 const StartUpCtrl = require("./start-up.ctrl");
 const startUp = express.Router();
+
+startUp.get("/", StartUpCtrl.getList);
+startUp.get("/:id", StartUpCtrl.getPost);
+startUp.delete("/:id", checkLoggedIn, StartUpCtrl.remove);
 
 module.exports = startUp;
--- src/api/start-up/start-up.ctrl.js
+++ src/api/start-up/start-up.ctrl.js
@@ -1,2 +1,30 @@
-exports.getList = async (req, res) => {};
-exports.remove = async (req, res) => {};
+const StartUp = require("../../model/start-up");
+
+exports.getList = async (req, res) => {
+  try {
+    const list = await StartUp.find();
+    res.send(list);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+exports.getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await StartUp.findById({ _id: id });
+    res.send(post);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+exports.remove = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await StartUp.findByIdAndDelete({ _id: id }).exec();
+    res.send("successfully remove");
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
