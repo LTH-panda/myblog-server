5d3d5a7834cdbd5daea80eada66f35555bb537ef
LTH <johtta888@naver.com>
2022-05-02 18:27:56 +0900

    add log api

--- src/api/log/index.js
+++ src/api/log/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const checkLoggedIn = require("../../lib/checkLoggedIn");
 const LogCtrl = require("./log.ctrl");
 const log = express.Router();
+
+log.get("/", LogCtrl.getList);
+log.delete("/", LogCtrl.remove);
 
 module.exports = log;
--- src/api/log/log.ctrl.js
+++ src/api/log/log.ctrl.js
@@ -1,2 +1,20 @@
-exports.getList = async (req, res) => {};
-exports.remove = async (req, res) => {};
+const Log = require("../../model/log");
+
+exports.getList = async (req, res) => {
+  try {
+    const list = await Log.find();
+    res.send(list);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+exports.remove = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const post = await Log.findByIdAndRemove({ _id: id }).exec();
+    res.send("successfully remove");
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
